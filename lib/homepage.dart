8536144e3be83a5f75004dbe8cf41ac942ac5f9b
prasannakumar2495 <prasannakumar2461995@gmail.com>
2021-06-07 15:10:23 +0530

    Added new pages and made UI changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:curequickly/signup.dart';
 import 'package:curequickly/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,80 +10,96 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter CureQuickly'),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(10),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  child: Image.asset(
-                    'assets/images/curequickly.jpeg',
-                  ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                  )
+                ],
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/curequickly.jpeg'),
                 ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  width: 400,
-                  child: TextField(
-                    keyboardType: TextInputType.phone,
-                    textAlign: TextAlign.center,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly,
-                    ],
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter Mobile Number',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  width: 400,
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter Password',
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Welcome(),
-                      ),
-                    );
-                  },
-                  child: Text('Submit'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueAccent,
-                    shadowColor: Colors.grey,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Flexible(
-                      child: Text('Don\'t have an Account?'),
-                      flex: 1,
-                    ),
-                    Flexible(
-                      child: TextButton(
-                        child: Text('Sign UP'),
-                        onPressed: () {},
-                      ),
-                      flex: 1,
-                    ),
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 400,
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                textAlign: TextAlign.center,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Mobile Number',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 400,
+              child: TextField(
+                keyboardType: TextInputType.text,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Password',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Welcome(),
+                  ),
+                );
+              },
+              child: Text('SIGN IN'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueAccent,
+                shadowColor: Colors.grey,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Flexible(
+                  child: Text('Don\'t have an Account?'),
+                  flex: 1,
+                ),
+                Flexible(
+                  child: TextButton(
+                    child: Text('SIGN UP'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUp(),
+                        ),
+                      );
+                    },
+                  ),
+                  flex: 1,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './survey.dart';
 
 class Welcome extends StatelessWidget {
   @override
@@ -6,22 +7,64 @@ class Welcome extends StatelessWidget {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                child: Text('Welcome'),
-                alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 32,
               ),
-              Text('Hello'),
-              Container(
-                child: Image.asset(
-                  'assets/images/waitinghall.jpeg',
+            ),
+            Text('Hello'),
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    //offset: Offset(0, 3),
+                  ),
+                ],
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/waitinghall.jpeg'),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. \nMost people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.  Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.',
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            Text(
+              'Please click on the below button to take the survey',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.blueGrey,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Survey(),
+                    ),
+                  );
+                },
+                child: Text('Survey'),
+              ),
+            ),
+          ],
         ),
       ),
     );
